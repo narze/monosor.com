c39d1aacb76200e826723ae386b8dbd08e594744
Manassarn Manoonchai <manassarn@gmail.com>
2020-07-31 00:33:22 +0700

    Use frontmatter title for html title

--- src/components/page-layout.js
+++ src/components/page-layout.js
@@ -6,12 +6,19 @@ import Footer from "./footer";
 
 import { MDXProvider } from "@mdx-js/react"
 import { Link } from "gatsby"
+import { Helmet } from "react-helmet"
 
 const shortcodes = { Link }
 
-function PageLayout({ children }) {
+function PageLayout({ children, pageContext }) {
+  const { title } = pageContext.frontmatter
+
   return (
     <div className="flex flex-col min-h-screen font-sans">
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+
       <Header />
 
       <main className="flex-grow w-full max-w-4xl px-4 py-2 mx-auto md:px-8 md:py-4">
@@ -25,6 +32,11 @@ function PageLayout({ children }) {
 
 PageLayout.propTypes = {
   children: PropTypes.node.isRequired,
+  pageContext: PropTypes.shape({
+    frontmatter: PropTypes.shape({
+      title: PropTypes.string.isRequired
+    })
+  })
 };
 
 export default PageLayout;
