509d9a60e2fd44b810fefab6c60366caca6e25aa
Manassarn Manoonchai <manassarn@gmail.com>
2020-07-31 09:18:43 +0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-5859500f51b9a33a5090.js"
+    "url": "webpack-runtime-2e0ad050a521391b43d1.js"
   },
   {
     "url": "framework-6d7c1fde620d33eccee3.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6d2af105d145dde6f1137eaeffd44a1d"
+    "revision": "6686cee1e82f8f8b2efb8da8ac00c186"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-7c31e2436cade51cbcda.js"
