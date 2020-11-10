d0a7ff95fbef57daa41c68d754824b7ea4a3f40b
Manassarn Manoonchai <manassarn@gmail.com>
2020-11-10 23:20:24 +0700

    feat: add mdx recipe

--- new/gatsby-config.js
+++ new/gatsby-config.js
@@ -3,5 +3,13 @@ module.exports = {
     "gatsby-plugin-postcss",
     "gatsby-plugin-typescript",
     "gatsby-plugin-eslint",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "MDXPages",
+        path: "src/pages/",
+      },
+    },
+    "gatsby-plugin-mdx",
   ],
 };
