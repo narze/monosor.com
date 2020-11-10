841eb087d9602fde21e03fb2f7dcd72c48f5fa96
Manassarn Manoonchai <manassarn@gmail.com>
2020-11-11 01:15:15 +0700

    feat: prettier, eslint, typescript

--- new/tailwind.config.js
+++ new/tailwind.config.js
@@ -2,10 +2,13 @@ module.exports = {
   future: {
     purgeLayersByDefault: true,
   },
-  purge: ["./src/**/*.{js,jsx,ts,tsx,mdx}"],
+  purge: ['./src/**/*.{js,jsx,ts,tsx,mdx}'],
   theme: {
     extend: {},
   },
   variants: {},
   plugins: [],
-};
+  corePlugins: {
+    preflight: false,
+  },
+}
